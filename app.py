19bc56fa474a319c3f44aa0878d334391ab4de1e
stephaneDoss <90639864+stephaneDoss@users.noreply.github.com>
2024-07-17 12:12:20 +0100

    chore: Update app.py to use rfm_online_retail.csv for RFM data

--- app.py
+++ app.py
@@ -7,18 +7,17 @@ import plotly.express as px
 # Charger les données RFM et les résultats du clustering
 # Assurez-vous d'avoir calculé les métriques RFM et les clusters K-means comme expliqué précédemment
 # Chargez votre propre fichier CSV ici
-rfm_with_demographics = pd.read_csv('rfm_with_demographics.csv')
+rfm = pd.read_csv('rfm_online_retail.csv')
 
 # Titre de l'application Streamlit
 st.title('Tableau de Bord RFM Clustering')
 
 # Sélection du cluster avec un menu déroulant
 selected_cluster_name = st.selectbox('Sélectionner un nom de cluster :', sorted(
-    rfm_with_demographics['Cluster Name'].unique()))
+    rfm['Cluster Name'].unique()))
 
 # Filtrer les données pour le nom de cluster sélectionné
-cluster_data = rfm_with_demographics[rfm_with_demographics['Cluster Name']
-                                     == selected_cluster_name]
+cluster_data = rfm[rfm['Cluster Name'] == selected_cluster_name]
 
 # Afficher le graphique interactif avec Plotly Express
 fig = px.scatter(cluster_data, x='Recency', y='Monetary', color='Cluster Name',
